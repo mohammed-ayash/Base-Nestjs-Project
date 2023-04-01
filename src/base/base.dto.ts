1d31c4e89b694dfa4ac4ff41ddef429244ad933f
Mohammed Ayash <mohammedayash@Mohammeds-MacBook-Pro.local>
2023-04-01 12:47:25 +0300

    fix bug in generic Crud generate swagger request response

--- src/base/base.dto.ts
+++ src/base/base.dto.ts
@@ -1,7 +1,5 @@
 import { ClassType } from './class.type';
 
 export function BaseRequestDto<T extends ClassType>(ResourceCls: T) {
-  class BaseDto extends ResourceCls {}
-
-  return BaseDto;
+  return ResourceCls;
 }
